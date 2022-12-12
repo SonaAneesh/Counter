6ff01b297c5e72da398cdb6ed1056a0838d8747a
SonaAneesh <sona87anu@gmail.com>
2022-12-12 12:46:33 +0400

    Js file added

---
+++ index.js
@@ -0,0 +1,32 @@
+// intialize the count
+let count = 0;
+
+// select value of counter and the buttons
+const value =document.querySelector('.value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn)=>{
+  btn.addEventListener('click',function(e){
+
+    const styles = e.currentTarget.classList;
+    if(styles.contains('decrease')){
+      count--; 
+    }
+    else if(styles.contains('increase')){
+      count++;
+    }else{
+      count = 0;
+    }
+    
+    if(count > 0){
+      value.style.color = "green";
+    }
+    if(count < 0){
+      value.style.color = "red";
+    }
+    if(count === 0){
+      value.style.color = "#02074f"
+    }
+    value.textContent = count;
+  })
+})
